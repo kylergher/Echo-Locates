4f7e09f95e888b0ccab35d98d82767362700403a
Kyler V <kylergher@lcad.edu>
2024-05-05 11:38:57 -0700

    it crashed. fuck

--- ECHO LOCATES/Assets/_myAssets/scripts/StartScreen.cs
+++ ECHO LOCATES/Assets/_myAssets/scripts/StartScreen.cs
@@ -1,32 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class StartScreen : MonoBehaviour
-
+public class StartScene: MonoBehaviour
 {
-public GameObject window;
-public void whenButtonClicked()
-{
-window.SetActive(false);
-}
+    public string gamePlay;
 
-
-
-
-
-
-
-
-    // Start is called before the first frame update
-    void Start()
+    public void LoadLevel()
     {
-        
+        SceneManager.LoadScene(gamePlay);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ExitGame()
     {
-        
+        Application.Quit();
     }
 }
